bc1a3db93878c38fac9de3c8fbc50416f6591d94
Joe Cross <joe.mcross@gmail.com>
2014-10-10 23:27:20 -0700

    Initial commit - project bootstrap

---
+++ README.rst
@@ -0,0 +1,23 @@
+bottom
+========
+:Build: |build|_ |coverage|_
+:Documentation: http://bottom.readthedocs.org/
+:Downloads: http://pypi.python.org/pypi/bottom
+:Source: https://github.com/numberoverzero/bottom
+
+.. |build| image:: https://travis-ci.org/numberoverzero/bottom.svg?branch=master
+.. _build: https://travis-ci.org/numberoverzero/bottom
+.. |coverage| image:: https://coveralls.io/repos/numberoverzero/bottom/badge.png?branch=master
+.. _coverage: https://coveralls.io/r/numberoverzero/bottom?branch=master
+
+bottom layer of the irc protocol for python's asyncio
+
+Installation
+============
+
+``pip install bottom``
+
+Getting Started
+===============
+
+There's nothing here yet!
---
+++ setup.py
@@ -0,0 +1,48 @@
+""" Setup file """
+import os
+import re
+from setuptools import setup, find_packages
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(HERE, 'README.rst')).read()
+CHANGES = open(os.path.join(HERE, 'CHANGES.rst')).read()
+# Remove custom RST extensions for pypi
+CHANGES = re.sub(r'\(\s*:(issue|pr|sha):.*?\)', '', CHANGES)
+
+REQUIREMENTS = [
+]
+
+TEST_REQUIREMENTS = [
+    'pytest',
+]
+
+if __name__ == "__main__":
+    setup(
+        name='bottom',
+        version='0.1.0',
+        description="bottom layer of the irc protocol for python's asyncio",
+        long_description=README + '\n\n' + CHANGES,
+        classifiers=[
+            'Development Status :: 2 - Pre-Alpha',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.4',
+            'Topic :: Software Development :: Libraries',
+            'Topic :: Communications :: Chat',
+            'Topic :: Communications :: Chat :: Internet Relay Chat'
+        ],
+        author='Joe Cross',
+        author_email='joe.mcross@gmail.com',
+        url='http://bottom.readthedocs.org/',
+        license='MIT',
+        keywords='meta metaclass declarative orm',
+        platforms='any',
+        include_package_data=True,
+        py_modules=['bottom'],
+        packages=find_packages(exclude=('tests',)),
+        install_requires=REQUIREMENTS,
+        tests_require=REQUIREMENTS + TEST_REQUIREMENTS,
+    )
