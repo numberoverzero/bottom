385bb629eb77a1a3ed6e736c5c18ccd7c0d035c1
Joe Cross <joe.mcross@gmail.com>
2016-01-02 12:04:16 -0800

    First pass at subclassing asyncio.Protocol

---
+++ bottom/protocol.py
@@ -0,0 +1,59 @@
+import asyncio
+from bottom.unpack import unpack_command
+DELIM = b"\r\n"
+
+
+class Protocol(asyncio.Protocol):
+    @classmethod
+    def factory(cls, client):
+        """Creates a factory method appropriate for create_connection which
+        returns an instance of the protocol bound to the given client.
+
+        Example
+        -------
+        import bottom
+
+        client = bottom.Client(...)
+        protocol_factory = Protocol.factory(client)
+        await client.loop.create_connection(protocol_factory, ...)
+        """
+        return lambda: cls(client)
+
+    def __init__(self, client):
+        self.client = client
+        self.transport = None
+        self.buffer = b""
+
+    def connection_made(self, transport):
+        self.transport = transport
+        self.client.trigger(
+            "client_connect", host=self.client.host, port=self.client.port)
+
+    def connection_lost(self, exc):
+        self.client.trigger(
+            "client_disconnect", host=self.client.host, port=self.client.port)
+        self.close()
+
+    def data_received(self, data):
+        self.buffer += data
+        # All but the last result of split should be pushed into the
+        # client.  The last will be b"" if the buffer ends on b"\r\n"
+        *lines, self.buffer = self.buffer.split(DELIM)
+        for line in lines:
+            message = line.decode(self.client.encoding, "ignore").strip()
+            try:
+                event, kwargs = unpack_command(message)
+                self.client.trigger(event, **kwargs)
+            except ValueError:
+                print("PARSE ERROR {}".format(message))
+
+    def write(self, message):
+        data = message.encode(self.client.encoding)
+        if not data.endswith(DELIM):
+            data += DELIM
+        self.transport.write(data)
+
+    def close(self):
+        if self.transport is not None:
+            self.transport.close()
+            self.transport = None
---
+++ tests/test_protocol.py
@@ -0,0 +1,82 @@
+from bottom.protocol import Protocol
+import pytest
+
+
+@pytest.fixture
+def protocol(client):
+    return Protocol(client)
+
+
+@pytest.fixture
+def transport():
+    class Transport:
+        def __init__(self):
+            self.written = []
+            self.closed = False
+
+        def write(self, data):
+            self.written.append(data)
+
+        def close(self):
+            self.closed = True
+    return Transport()
+
+
+def test_connect(protocol, transport, client):
+    protocol.connection_made(transport)
+    assert client.triggers["CLIENT_CONNECT"] == 1
+
+
+def test_disconnect_before_connect(protocol, transport, client):
+    protocol.connection_lost(transport)
+    assert client.triggers["CLIENT_DISCONNECT"] == 1
+
+
+def test_disconnect_after_connect(protocol, transport, client):
+    protocol.connection_made(transport)
+    protocol.connection_lost(transport)
+    assert client.triggers["CLIENT_CONNECT"] == 1
+    assert client.triggers["CLIENT_DISCONNECT"] == 1
+    assert transport.closed
+
+
+def test_write(protocol, transport):
+    protocol.connection_made(transport)
+    protocol.write("hello")
+    protocol.write("world\r\n")
+    protocol.write("\r\nfoo\r\n")
+    assert transport.written == [b"hello\r\n", b"world\r\n", b"\r\nfoo\r\n"]
+
+
+def test_partial_line(protocol, transport, client):
+    """Part of an IRC line is sent across; shouldn't be emitted as an event"""
+    protocol.data_received(b":nick!user@host PRIVMSG")
+    assert not client.triggers["PRIVMSG"]
+
+
+def test_multipart_line(protocol, transport, client):
+    """Single line transmitted in multiple parts"""
+    protocol.data_received(b":nick!user@host PRIVMSG")
+    protocol.data_received(b" #target :this is message\r\n")
+    assert client.triggers["PRIVMSG"] == 1
+
+
+def test_multiline_chunk(protocol, transport, client):
+    """Multiple IRC lines in a single data_received block"""
+    protocol.data_received(
+        b":nick!user@host PRIVMSG #target :this is message\r\n" * 2)
+    assert client.triggers["PRIVMSG"] == 2
+
+
+def test_invalid_line(protocol, transport, client):
+    """Well-formatted but invalid line"""
+    protocol.data_received(b"blah unknown command\r\n")
+    assert not client.triggers
+
+
+def test_factory(client):
+    """Protocol.factory returns a function that is
+    suitable for loop.create_connection, with a reference to the client"""
+    factory = Protocol.factory(client)
+    for _ in range(10):
+        assert factory().client is client
